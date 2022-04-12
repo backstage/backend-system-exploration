ebde4b0e859e34314aa58e2ef93d2577f5d753ff
Patrik Oldsberg <poldsberg@gmail.com>
2022-04-12 16:33:49 +0200

    finish the frontification
    Co-authored-by: Fredrik Adelöw <freben@gmail.com>
    Co-authored-by: blam <ben@blam.sh>
    Co-authored-by: Johan Haals <johan.haals@gmail.com>

--- experiments/frontendification/packages/backend/src/index.ts
+++ experiments/frontendification/packages/backend/src/index.ts
@@ -13,36 +13,66 @@ Stuff to explore:
 
 */
 
-const catalog = createBackendPlugin({
-  register(derp, hooks) {
-    pluginRouter.use('/catalog', router);
-
-    hooks.initialize.tapAsync(async () => {
-      // async initialization
-    });
-
-    hooks.shutdown.tapAsync(async () => {
-      // async shutdown
-    });
-  },
-});
+import { commonEnvBundle } from '@internal/backend-env';
+import { authPlugin } from '@backstage/plugin-auth-backend';
+import { catalogPlugin } from '@backstage/plugin-catalog-backend';
+import {
+  scaffolderPlugin,
+  ScaffolderEntitiesProcessor,
+} from '@backstage/plugin-scaffolder-backend';
 
 const backend = await createBackend({
-  apis: [
+  bundles: [commonEnvBundle()],
+  configures: [
+    {
+      api: loggerApiRef,
+      deps: { config: configApiRef },
+      configure: (logger, { config }) => {
+        logger.setLogLevel(config.getString('...'));
+      },
+    },
+    ScaffolderEntitiesProcessor.configurator(),
+    // ===
+    {
+      api: catalogPlugin,
+      deps: { logger: loggerApiRef },
+      configure: (catalogPlugin, { logger }) => {
+        catalogPlugin.addProcessor(new ScaffolderEntitiesProcessor(logger));
+      },
+    },
+  ],
+  provides: [
     createApiFactory({
       api: serviceDiscoveryApiRef,
       deps: { logger: loggerApiRef },
-      factory: ({ logger }) => createDiscovery(logger),
+      factory: async ({ logger }) => createServiceDiscovery(logger),
     }),
     createApiFactory(loggerApiRef, createCustomLogger()),
+    createApiFactory(catalogRefreshApi, createCustomRefresher()),
   ],
   plugins: [auth, catalog, scaffolder],
 });
 
 // OR
 
-backend.add(auth);
-backend.add(catalog);
-backend.add(scaffolder);
+backend.add(env);
+
+backend.add(authPlugin);
+backend.add(catalogPlugin, { apis: [] });
+backend.add(scaffolderPlugin);
 
 await backend.start(); // Maybe internally split into prepare + start?
+
+// Catalog plugin implementation example
+const catalog = createBackendPlugin({
+  apis: [
+    {
+      api: catalogRefreshApi,
+      deps: { logger: loggerApiRef },
+      factory: ({ logger }) => createRefreshApi(logger),
+    },
+  ],
+  register(holder, hooks) {
+    holder.getApi(routeApiRef).use('/catalog', router);
+  },
+});
