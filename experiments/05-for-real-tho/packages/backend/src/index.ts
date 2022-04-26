2f95297623f519159315611d2ca9756c2f5b436d
Patrik Oldsberg <poldsberg@gmail.com>
2022-04-26 18:00:51 +0200

    experiments: add for-real-tho
    Co-authored-by: Fredrik Adelöw <freben@gmail.com>
    Co-authored-by: blam <ben@blam.sh>
    Co-authored-by: Johan Haals <johan.haals@gmail.com>

---
+++ experiments/05-for-real-tho/packages/backend/src/index.ts
@@ -0,0 +1,122 @@
+// @ts-nocheck
+
+import { commonEnvBundle } from '@internal/backend-env';
+import { authPlugin } from '@backstage/plugin-auth-backend';
+import { catalogPlugin } from '@backstage/plugin-catalog-backend';
+import {
+  scaffolderPlugin,
+  ScaffolderEntitiesProcessor,
+} from '@backstage/plugin-scaffolder-backend';
+
+/*
+SCOPE:
+
+plugin:
+ - auth
+   - with builtin resolver configured
+ - catalog
+   - adds github discovery of org and components
+   - adds scaffolder/template features
+ - scaffolder
+   - adds custom action
+   - consumes catalog client
+
+
+LATER SCOPE:
+  - plugin endpoint discovery replaced with custom version
+  - custom express middleware around it all
+*/
+
+interface Backend {
+  add(x: Plugin | Module): void;
+  start(): Promise<void>;
+}
+
+async function createBackend({ env = process.env }) {
+  require(resolveOwn('package.json')).dependencies;
+}
+
+const backend = await createBackend({
+  modules: 'auto',
+});
+
+backend.add(
+  authPlugin({
+    providers: {
+      github: {
+        signIn: {
+          resolver: authPlugin.providers.github.resolvers.nameMatchingName,
+        },
+      },
+    },
+  })
+);
+
+backend.add(authPlugin());
+backend.add(
+  githubAuthModule.signIn({
+    resolver: githubAuthModule.resolvers.nameMatchingName,
+  })
+);
+
+catalogPlugin.hooks.initProcessors.tap(async (x, next) => {
+  await next();
+});
+
+backend.add(catalogPlugin());
+backend.add(catalogPlugin.searchExtensions());
+backend.add(catalogGithubModule.orgDiscovery());
+backend.add(catalogGithubModule.entityDiscovery());
+
+backend.add(scaffolderPlugin());
+backend.add(scaffolderPlugin.catalogExtensions());
+
+await backend.start();
+
+////////////////////////////////////////////////////////////////////////////////
+
+await startBackend({
+  modules: [
+    customEnvStuff(),
+    authProvide({
+      providers: {
+        github: {
+          signIn: {
+            resolver: authPlugin.providers.github.resolvers.nameMatchingName,
+          },
+        },
+      },
+    }),
+    catalogPlugin(),
+    catalogPlugin.searchExtensions(),
+    catalogGithubModule.orgDiscovery(),
+    catalogGithubModule.entityDiscovery(),
+    scaffolderPlugin(),
+    scaffolderPlugin.catalogExtensions(),
+  ],
+});
+
+// backend.add(catalog({
+//   providers: [githubDiscoveryProvider()],
+//   processors: [...catalog.defaultProcessors(), ...scaffolder.defaultProcessors()]
+// }))
+
+// backend.add(scaffolder({
+//   actions: [...scaffolder.defaultActions(), myCustomAction()]
+// }));
+
+// scaffolderV3TemplateProcessor = createModule({
+//   envs: { catalog: catalogEnv },
+//   init({ catalog }) {
+//     catalog.initProcessors.tap((processors) => {
+//       processors.add(...)
+//     })
+//   }
+// })
+
+// backend.add(catalogPlugin({ processors: [scaffolderPlugin.processor()]}))
+
+// await startDefaultService(backend.build());
+
+// what about listen etc - is the server different from the service, so to speak
+// think more about health checks? maybe plugins register into a central facility
